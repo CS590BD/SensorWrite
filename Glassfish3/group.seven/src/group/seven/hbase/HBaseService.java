f44796c18d4ec2594d629e43f2faf955818918a7
JW Clark <this.clark@gmail.com>
2014-07-01 11:35:54 -0500

    add lab 3 report

--- Glassfish3/group.seven/src/group/seven/hbase/HBaseService.java
+++ Glassfish3/group.seven/src/group/seven/hbase/HBaseService.java
@@ -1,11 +1,8 @@
 package group.seven.hbase;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,7 +10,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.hadoop.conf.Configuration;
@@ -31,19 +27,30 @@ import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
-import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
-import com.sun.xml.rpc.processor.modeler.j2ee.xml.string;
-
 @Path("/hbase")
 public class HBaseService {
-		
-	private final String HBASE_ZOOKEEPER_QUORUM_IP = "localhost.localdomain"; //Modify this in your hosts file. It will already work on the UMKC server
+
+	private final String HBASE_ZOOKEEPER_QUORUM_IP = "localhost.localdomain"; // Modify
+																				// this
+																				// in
+																				// your
+																				// hosts
+																				// file.
+																				// It
+																				// will
+																				// already
+																				// work
+																				// on
+																				// the
+																				// UMKC
+																				// server
 	private final String HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT = "2181";
 	private final String HBASE_MASTER = HBASE_ZOOKEEPER_QUORUM_IP + ":60010";
 
 	/**
 	 * CREATE TABLE
-	 * http://localhost.localdomain:8080/goup.seven/rest/hbase/create/tablename/column1:column2:column3:column4:column5
+	 * http://localhost.localdomain:8080/goup.seven/rest/hbase/create
+	 * /tablename/column1:column2:column3:column4:column5
 	 * 
 	 * @param tablename
 	 * @param columnFamilies
@@ -52,8 +59,7 @@ public class HBaseService {
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/create/{tablename:.+}/{columnFamilies:.+}")
-	public String createTable(
-			@PathParam("tablename") String tablename,
+	public String createTable(@PathParam("tablename") String tablename,
 			@PathParam("columnFamilies") String columnFamilies) {
 		String line = "{'status':'init'}";
 		HBaseAdmin hba = null;
@@ -69,7 +75,7 @@ public class HBaseService {
 				hba = new HBaseAdmin(config);
 				hba.createTable(ht);
 				line = "{'status':'ok'}";
-			} catch (Exception ex){
+			} catch (Exception ex) {
 				line = exceptionToJson(ex);
 			}
 		} finally { // close the connection
@@ -85,62 +91,64 @@ public class HBaseService {
 	/**
 	 * READ ALL FROM TABLE
 	 * http://localhost:8080/group.seven/rest/hbase/fetch/tablename
+	 * 
 	 * @param table
 	 * @return
 	 */
-    @GET
+	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/fetch/{tablename:.+}")
-    public String readAll(@PathParam("tablename") String table) {
-    	String line = "";
-        Configuration config = getHBaseConfiguration();
- 		try{
-             HTable ht = new HTable(config, table);
-             Scan s = new Scan();
-             ResultScanner ss = ht.getScanner(s);
-             for(Result r:ss){
-                 for(KeyValue kv : r.raw()){
-                	 line = line+ new String(kv.getRow()) + " ";
-                	 line = line + new String(kv.getFamily()) + ":";
-                	 line = line + new String(kv.getQualifier()) + " ";
-                	 line = line + kv.getTimestamp() + " ";
-                	 line = line + new String(kv.getValue());
-                	 line = line + "\n\n";
-                 }
-             }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+	public String readAll(@PathParam("tablename") String table) {
+		String line = "";
+		Configuration config = getHBaseConfiguration();
+		try {
+			HTable ht = new HTable(config, table);
+			Scan s = new Scan();
+			ResultScanner ss = ht.getScanner(s);
+			for (Result r : ss) {
+				for (KeyValue kv : r.raw()) {
+					line = line + new String(kv.getRow()) + " ";
+					line = line + new String(kv.getFamily()) + ":";
+					line = line + new String(kv.getQualifier()) + " ";
+					line = line + kv.getTimestamp() + " ";
+					line = line + new String(kv.getValue());
+					line = line + "\n\n";
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return line;
-    }
-	
-    /**
-     * UPDATE AT QUALIFIER
-	 * http://localhost.localdomain:8080/goup.seven/rest/hbase/insert/tablename/row/family/qualifier
-     * @param value - passed in the header message
-     * @param table
-     * @param row
-     * @param family
-     * @param qualifier
-     * @return
-     */
+	}
+
+	/**
+	 * UPDATE AT QUALIFIER
+	 * http://localhost.localdomain:8080/goup.seven/rest/hbase
+	 * /insert/tablename/row/family/qualifier
+	 * 
+	 * @param value
+	 *            - passed in the header message
+	 * @param table
+	 * @param row
+	 * @param family
+	 * @param qualifier
+	 * @return
+	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/insert/{table:.+}/{row:.+}/{family:.+}/{qualifier:.+}")
-	public String insertSingle(
-			String value,
-			@PathParam("table") String table,
-			@PathParam("row") String row,
-			@PathParam("family") String family,
+	public String insertSingle(String value, @PathParam("table") String table,
+			@PathParam("row") String row, @PathParam("family") String family,
 			@PathParam("qualifier") String qualifier) {
-		
+
 		String line = "{'status':'init'}";
 		Configuration config = getHBaseConfiguration();
 		HTable ht = null;
 		try {
 			ht = new HTable(config, table);
 			Put put = new Put(Bytes.toBytes(row));
-			put.add(Bytes.toBytes(family), Bytes.toBytes(qualifier), Bytes.toBytes(value));
+			put.add(Bytes.toBytes(family), Bytes.toBytes(qualifier),
+					Bytes.toBytes(value));
 			ht.put(put);
 			line = "{'status':'ok'}";
 		} catch (Exception ex) {
@@ -148,17 +156,17 @@ public class HBaseService {
 		}
 		return line;
 	}
-	
+
 	/**
 	 * DELETE TABLE
+	 * 
 	 * @param table
 	 * @return
 	 */
 	@DELETE
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/delete/{table:.+}")
-	public String deleteTable(
-			@PathParam("table") String table) {
+	public String deleteTable(@PathParam("table") String table) {
 		String line = "{'status':'init}";
 		try {
 			Configuration config = getHBaseConfiguration();
@@ -171,10 +179,12 @@ public class HBaseService {
 		}
 		return line;
 	}
-	
+
 	/**
 	 * INSERT FILE
-	 * http://localhost:8080/group.seven/rest/hbase/insert-sensor-txt/ttjj_sensor_txt//path
+	 * http://localhost:8080/group.seven/rest/hbase/insert-sensor-txt
+	 * /ttjj_sensor_txt//path
+	 * 
 	 * @param tablename
 	 * @param filepath
 	 * @param columnFamilies
@@ -183,10 +193,9 @@ public class HBaseService {
 	@GET
 	@Produces("application/json")
 	@Path("/insert-sensor-txt/{tablename:.+}/{filepath:.+}")
-	public String insertFile(
-			@PathParam("tablename") String table,
+	public String insertFile(@PathParam("tablename") String table,
 			@PathParam("filepath") String filePath) {
-		
+
 		String line = "{'status':'init'}";
 		try {
 			HBase.insertSensorsTxt(table, filePath);
@@ -196,30 +205,32 @@ public class HBaseService {
 		}
 		return line;
 	}
-	
+
 	/**
 	 * RETURN THE HBASE CONFIGURATION
+	 * 
 	 * @return
 	 */
 	private Configuration getHBaseConfiguration() {
 		Configuration config = HBaseConfiguration.create();
 		config.clear();
 		config.set("hbase.zookeeper.quorum", HBASE_ZOOKEEPER_QUORUM_IP);
-		config.set("hbase.zookeeper.property.clientPort", HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT);
+		config.set("hbase.zookeeper.property.clientPort",
+				HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT);
 		config.set("hbase.master", HBASE_MASTER);
 		return config;
 	}
-	
-	
+
 	/**
 	 * HANDLE ALL EXCEPTIONS
+	 * 
 	 * @param ex
 	 * @return
 	 */
 	private String exceptionToJson(Exception ex) {
 		String json = "{'status':'fail','exception':'";
 		String exception = "";
-		String message = "";	
+		String message = "";
 		if (ex instanceof MasterNotRunningException) {
 			exception = MasterNotRunningException.class.toString();
 			message = ex.getMessage();
